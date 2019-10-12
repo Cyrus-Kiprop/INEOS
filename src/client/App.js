b79df10fe6471e6ebc5e90327223200e62b24db2
@cyrusKip <kip@localhost.localdomain>
2019-10-12 22:47:07 +0300

    design login UI

--- src/client/App.js
+++ src/client/App.js
@@ -1,40 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import './app.css';
-import Card from './card';
+import React, { useState, useEffect } from "react";
+import "./app.css";
+
+import Login from "./components/login";
+// import Card from './card';
 
 const App = () => {
-  const [data, setdata] = useState(null);
-
-  async function fetchProduct() {
-    const response = await fetch('/api/getUsername');
-    const json = await response.json();
-    setdata(json);
-  }
-
-  useEffect(() => {
-    fetchProduct();
-  }, []);
-
   return (
-    <div>
-      {data && data.map((info) => {
-        const { title, date, text, _id } = info;
-
-        return (
-          // eslint-disable-next-line no-underscore-dangle
-          <div key={info._id}>
-            <Card
-              id={_id}
-              title={title}
-              date={date}
-              text={text}
-            />
-          </div>
-        );
-      })
-    }
-    </div>
-
+    <>
+      <Login />
+    </>
   );
 };
 export default App;
---
+++ src/client/components/login/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./index.css";
+// import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <div className="login-container">
+      <form action="index.html" className="box">
+        <h3>Fika Safe</h3>
+        <h5>Welcome back! Please login into your account</h5>
+        <input type="text" name="" placeholder="Username" />
+        <input type="password" name="" placeholder="Password" />
+        <span>
+          <input type="checkbox" name="" />
+          <label>Remember me</label>
+          <a href="#!">Forgot Password</a>
+        </span>
+
+        <input type="submit" name="" value="login" />
+        <input type="submit" name="" value="Sign up" />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
